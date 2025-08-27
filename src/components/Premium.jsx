de58a88bf173f5751c439562dbc1ac4b87889b57
Bikash <bikashkdas23@gmail.com>
2025-08-27 12:02:28 +0530

    Add useEffect import to Premium component for user verification

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../utils/constants";
 
 const Premium = () => {
